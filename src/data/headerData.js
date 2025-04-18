96605b1c6256fabb4909d6f36a359c5548b60b74
Kishore <40794060+kishore1919@users.noreply.github.com>
2025-04-18 09:04:06 +0000

    Added header changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import image from '../assets/png/kishore.jpg'
 export const headerData = {
     name: 'Kishore Selvaraj',
     title: "Site Reliability Engineer",
-    desciption:"I've missed more than 9,000 shots in my career. I've lost almost 300 games. Twenty-six times I've been trusted to take the game-winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed - Michael Jordan",
+    desciption:"Our greatest glory is not in never failing, but in rising up every time we fail",
     image: "https://portfoliodata-2022.s3.amazonaws.com/Kishore_passport_size.jpg",
     resumePdf: "https://portfoliodata-2022.s3.amazonaws.com/Kishore+selvaraj.pdf"
 }
