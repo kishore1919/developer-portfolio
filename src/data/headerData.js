f08f2906865e82675a086507f65d1d27aaa15750
Kishore <40794060+kishore1919@users.noreply.github.com>
2025-04-18 09:14:24 +0000

    Added header changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import image from '../assets/png/kishore.jpg'
 export const headerData = {
     name: 'Kishore Selvaraj',
     title: "Site Reliability Engineer",
-    desciption:"Our greatest glory is not in never failing, but in rising up every time we fail",
+    desciption:"Dream big, work hard, and let your passion guide you. Success isn’t always immediate, but resilience is key. Remember, every failure is a lesson, every setback is a chance to grow. Embrace challenges; they shape your destiny. Keep moving forward, always",
     image: "https://portfoliodata-2022.s3.amazonaws.com/Kishore_passport_size.jpg",
     resumePdf: "https://portfoliodata-2022.s3.amazonaws.com/Kishore+selvaraj.pdf"
 }
